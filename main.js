01be1cf536320dcc03924a3e86a40aef134229cd
katy millard <katy.millard@gmail.com>
2022-01-24 11:18:03 -0600

    Added while loop.

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ import input from 'input';
 
 async function main() {
     const answer = 'Katy is Cool';
-    const guess = await input.text('Guess the password');
+    let guess = await input.text('Guess the password');
+
+    while (guess != answer) {
+        guess = await input.text('Guess the password');
+    }
     
     if (guess == answer) {
         console.log('You are smart.');
-    } else {
-        console.log('Try again.')
     }
 }
 
 main()
-    
